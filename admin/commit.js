2a84b2ce04cfffa755297ba6cdc9b725c0572be0
ccy <303752792@qq.com>
2019-06-27 22:23:07 +0800

    1

---
+++ admin/commit.js
@@ -0,0 +1,24 @@
+API = 'http://45.40.196.130:16327/admin'
+clientHttp = function (url, data, method, callback) {
+    var data_1 = data
+    if (method == 'POST' || method == 'PUT') {
+        data_1 = JSON.stringify(data);
+    }
+    $.ajax({
+        url: API + url,
+        type: method,
+        data: data_1,
+        headers: {
+            'Content-Type': "application/json;charset=utf-8"
+        },
+        xhrFields: {
+            withCredentials: true // 这里设置了withCredentials
+        },
+        success: function (res) {
+            callback.call(this, res)
+        },
+        error: function () {
+            layer.msg('系统错误');
+        }
+    })
+}
